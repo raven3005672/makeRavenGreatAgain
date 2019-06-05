478fe6397d82e9c52db9f97bf0445d9430cbb573
yanglinylin.yang <yanglinylin.yang@qunar.com>
2019-06-05 21:54:30 +0800

    .

--- yygmind/1.stack/1-1.js
+++ yygmind/1.stack/1-1.js
@@ -141,8 +141,5 @@ c = multiply(20, 30);
 
 
 
-
-
-
-
-
+// https://heyingye.github.io/2018/03/19/js%E5%BC%95%E6%93%8E%E7%9A%84%E6%89%A7%E8%A1%8C%E8%BF%87%E7%A8%8B%EF%BC%88%E4%B8%80%EF%BC%89/
+// 结合此篇理解
