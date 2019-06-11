46a58ebcddbe011a3c456d25011d32198df8ef42
yanglinylin.yang <yanglinylin.yang@qunar.com>
2019-06-11 21:41:13 +0800

    .

---
+++ yygmind/2.scope/2-1.js
@@ -0,0 +1,46 @@
+// 闭包是指有权访问另外一个函数作用域中的变量的函数
+// 关键在于：是一个函数、能访问另外一个函数作用域中的变量
+
+// 关于闭包有下面三个特性
+// 1、闭包可以访问当前函数以外的变量
+function getOuter() {
+    var date = '815';
+    function getDate(str) {
+        console.log(str + date);       // 访问外部的date
+    }
+    return getDate('今天是: ');         // 今天是：815
+}
+getOuter();
+// 2、即使外部函数已经返回，闭包仍能访问外部函数定义的变量
+function getOuter() {
+    var date = '815';
+    function getDate(str) {
+        console.log(str + date);        // 访问外部的date
+    }
+    return getDate;                     // 外部函数返回
+}
+var today = getOuter();
+today('今天是: ');          // 今天是：815
+today('明天不是: ');        // 明天不是： 815
+// 3、闭包可以更新外部变量的值
+function updateCount() {
+    var count = 0;
+    function getCount(val) {
+        count = val;
+        console.log(count);
+    }
+    return getCount;        // 外部函数返回
+}
+var count = updateCount();
+count(815);         // 815
+count(816);         // 816
+
+
+// 作用域链
+// javascript中有一个执行上下文(execution context)的概念，它定义了变量或函数有权访问的其它数据，决定了他们各自的行为。每个执行环境都有一个与之关联的变量对象，环境中定义的所有变量和函数都保存在这个对象中。
+
+
+
+
+
+
